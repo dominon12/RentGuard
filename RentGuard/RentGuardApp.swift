a3083d050290d08e106c7d427bd6b9c852a5642f
Maksim Sobolev <sobolevmax10@gmail.com>
2024-02-27 17:00:56 +0100

    Initial Commit

---
+++ RentGuard/RentGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentGuardApp.swift
+//  RentGuard
+//
+//  Created by Maksim Sobolev on 27/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RentGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
