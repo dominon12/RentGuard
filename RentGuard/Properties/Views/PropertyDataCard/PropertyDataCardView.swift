1774398c3ac02e8fd1727047b7624ece6c1ec1b3
Maksim Sobolev <sobolevmax10@gmail.com>
2024-03-07 18:59:31 +0100

    Display correct address in PropertyDataCardView

--- RentGuard/Properties/Views/PropertyDataCard/PropertyDataCardView.swift
+++ RentGuard/Properties/Views/PropertyDataCard/PropertyDataCardView.swift
@@ -11,10 +11,24 @@ struct PropertyDataCardView: View {
     let property: Property
     @Binding var isShowingForm: Bool
     
+    var address: String {
+        var address = property.address
+        if let city = property.city {
+            address += ", " + city
+        }
+        if let postalCode = property.postalCode {
+            address += ", " + postalCode
+        }
+        if let country = property.country {
+            address += ", " + country
+        }
+        return address
+    }
+    
     var body: some View {
         DataCardView(title: "Data",
                      data: [
-                        ("Address", property.address),
+                        ("Address", address),
                         ("Surface", property.surface ?? "-"),
                         ("Registration Id", property.registrationId ?? "-"),
                         ("Price", property.price ?? "-")
